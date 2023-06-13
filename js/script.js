dd7079e1f19c139d324a77029a943fba28ea8379
gomez50057 <37481091+gomez50057@users.noreply.github.com>
2023-06-13 11:54:19 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ for (var i = 0; i < arrow.length; i++) {
 }
 
 let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector("#logo");
+let sidebarBtn = document.querySelector(".logo");
 console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
